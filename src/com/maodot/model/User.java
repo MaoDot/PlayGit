ff802a4ea594b8ea1cd464cae6bd8f2061f0270f
MaoDot <maodot@126.com>
2017-09-26 16:32:12 +0800

    add model class User

---
+++ src/com/maodot/model/User.java
@@ -0,0 +1,32 @@
+package com.maodot.model;
+
+/**
+ * 用户类
+ * @author Administrator
+ *
+ */
+public class User {
+	private String name;
+	private Integer age;
+	private String phoneNumber;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+}
